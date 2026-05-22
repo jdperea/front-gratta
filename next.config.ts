45293fc35e2595dd80a80fe9e061462d94d41f10
Juan Diego Perea Valencia <jdperea59@gmail.com>
2026-05-21 23:04:00 -0500

    added cors origins

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.1.4"], 
+  allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.1.4", "gratta-eight.vercel.app", "gratta-k2m2.vercel.app"], 
 };
 
 export default nextConfig;
